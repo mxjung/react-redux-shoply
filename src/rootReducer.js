4bc5094225e87cc8c54e356236f6bfc68bf219a2
Max Jung <mjung@Peters-MacBook-Pro.local>
2020-05-05 13:01:44 -0700

    fixed rootReducer to handle add_item

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,25 +1,23 @@
 const { products } = require('./data.json');
-// import * as data from './data.json';
-// const { products } = data;
 
 // cart: Object.keys(products).map(key => ({ [key]: { "count": 0 } })),
 
 const INITIAL_STATE = {
-  cart: [],
+  // cart: [],
+  cart: Object.keys(products).map(productId => ({ id: productId, "count": 0 })),
   inventory: products
 };
 
 function rootReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case "ADD_ITEM":
-      // action.payload = id
       return {
         ...state,
         cart: state.cart.map(itemObj => {
           if (itemObj.id === action.payload) {
-            return { [itemObj.id]: { count: state.cart[itemObj.id].count + 1 } }
+            return { id: itemObj.id, count: itemObj.count + 1 };
           } else {
-            return { [action.payload]: { count: 1 } };
+            return { ...itemObj };
           }
         })
       }
